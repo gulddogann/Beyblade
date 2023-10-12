0f158ca83d0632d7403d9bfd535bf507060412a3
gülüzar doğan <dogangul778@gmail.com>
2023-10-12 13:58:14 +0300

    Beyblade

---
+++ src/BeybladeFabrikasi.java
@@ -0,0 +1,29 @@
+public class BeybladeFabrikasi {
+    public Beyblade beybladeUret(String beyblade_turu) {
+
+        if (beyblade_turu.equals("Dragon")){
+            return new Dragon("Takao", 700, 500,"Mavi Ejderha","Kutsal Canavarla Konuşma");
+
+        }
+        else if (beyblade_turu.equals("Dranza")) {
+
+            return new Dranza("Kai",500,200,"Kırmızı Anka Kuşu");
+
+        }
+        else if (beyblade_turu.equals("Drayga")) {
+
+            return new Dranza("Rei",800,250,"Beyaz Kaplan");
+
+        }
+        else if (beyblade_turu.equals("Draciel")) {
+
+            return new Dranza("Max",400,1000,"Kara Kaplumbağa");
+
+        }
+        else {
+            return null;
+
+        }
+
+    }
+}
